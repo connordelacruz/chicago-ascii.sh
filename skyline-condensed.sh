2ea4f278c5996a7a9a58b135a2eab74b0224ea73
Connor de la Cruz <connor.c.delacruz@gmail.com>
2021-10-26 15:16:12 -0500

    Made skyline-condensed.sh smarter about how it handles custom colors
    - If none are provided, alternate 2-color default
    - If 1 is provided, go monochrome
    - If 2-5 are provided, repeat pattern once it ends (e.g. if 3 are
      provided, repeat colors 1-3 for 4-6)

--- skyline-condensed.sh
+++ skyline-condensed.sh
@@ -1,52 +1,72 @@
 #!/usr/bin/env bash
 
-# Default color escape sequences (left to right)
-c1="$(tput setaf 4)"
-c2="$(tput setaf 6)"
-c3="$(tput setaf 4)"
-c4="$(tput setaf 6)"
-c5="$(tput setaf 4)"
-c6="$(tput setaf 6)"
-# Reset attributes
-rst="$(tput sgr0)"
+main() {
+    # Colors ===================================================================
+    # TODO: just use positional args instead
+    # Iterate thru args, assign variables c0-c5
+    max_colors=6
+    i=0
+    while getopts "c:" opt; do
+        case $opt in
+            c)
+                eval "c$i=\$(tput setaf $OPTARG)"
+                ((i++))
+                if [[ $i == $max_colors ]]; then
+                    break
+                fi
+                ;;
+        esac
+    done
+    # If no colors were passed, default to blue and cyan
+    if [[ $i == 0 ]]; then
+        # Default: blue
+        c0="$(tput setaf 4)"
+        # Default: cyan
+        c1="$(tput setaf 6)"
+        # Update i so code below can fill in remaining colors
+        i=2
+    fi
+    # Handle any unset color vars
+    if (( i < max_colors )); then
+        for (( x=$i; x<$max_colors; x++ )); do
+            eval "c${x}=\$c$(( x % i ))"
+        done
+    fi
 
-# if -c flag is provided, override default colors
-i=1
-while getopts "c:" opt; do
-    case $opt in
-        c)
-            eval "c$i=\$(tput setaf $OPTARG)"
-            ((i++))
-            ;;
-    esac
-done
+    # Formatting ===============================================================
+    # Reset attributes
+    rst="$(tput sgr0)"
+    # Centering text
+    # Width of output
+    width=31
+    cols=$(tput cols)
+    # Padding
+    space_count=$(( ($cols/2) - ($width/2) ))
+    pad=$(printf '%*s' "$space_count" | tr ' ' ' ')
 
-# Centering text
-# Width of output
-width=31
-cols=$(tput cols)
-# Padding
-space_count=$(( ($cols/2) - ($width/2) ))
-pad=$(printf '%*s' "$space_count" | tr ' ' ' ')
+    # Print Art ================================================================
 
-cat << EOF
+    cat << EOF
 
-${pad}          ${c3}   ╷ ╷${rst}
-${pad}          ${c3}   │ │${rst}
-${pad}          ${c3}   ║ ║${rst}
-${pad}          ${c3}   ║ ║${rst}
-${pad}          ${c3}  ▐███▌${rst}
-${pad}          ${c3}  ▐███▌     ${c5} │   │${rst}
-${pad}          ${c3}  ▐███▌     ${c5} ╽   ╽${rst}
-${pad}          ${c3} ▐█████▌    ${c5} ┃   ┃${rst}
-${pad}          ${c3} ▐█████▌    ${c5} █████${rst}
-${pad}   ${c2}  ▄██▄ ${c3} ▐█████▌    ${c5} █████${rst}
-${pad}   ${c2}  ████ ${c3}▐███████▌   ${c5} █████${rst}
-${pad}   ${c2}  ████ ${c3}▐███████▌   ${c5}▐█████▌${rst}
-${pad}   ${c2}▐██████${c3}▐███████▌   ${c5}▐█████▌${rst}
-${pad}   ${c2}▐██████${c3}▐████${c4}▐█████▌${c5}▐█████▌${rst}
-${pad}   ${c2}▐██████${c3}▐████${c4}▐█████▌${c5}███████${rst}
-${pad}${c1}█████▌${c2}████${c3}▐████${c4}▐█████▌${c5}████${c6}▐████${rst}
-${pad}${c1}█████▌${c2}████${c3}▐████${c4}▐█████▌${c5}████${c6}▐████${rst}
+${pad}          ${c2}   ╷ ╷${rst}
+${pad}          ${c2}   │ │${rst}
+${pad}          ${c2}   ║ ║${rst}
+${pad}          ${c2}   ║ ║${rst}
+${pad}          ${c2}  ▐███▌${rst}
+${pad}          ${c2}  ▐███▌     ${c4} │   │${rst}
+${pad}          ${c2}  ▐███▌     ${c4} ╽   ╽${rst}
+${pad}          ${c2} ▐█████▌    ${c4} ┃   ┃${rst}
+${pad}          ${c2} ▐█████▌    ${c4} █████${rst}
+${pad}   ${c1}  ▄██▄ ${c2} ▐█████▌    ${c4} █████${rst}
+${pad}   ${c1}  ████ ${c2}▐███████▌   ${c4} █████${rst}
+${pad}   ${c1}  ████ ${c2}▐███████▌   ${c4}▐█████▌${rst}
+${pad}   ${c1}▐██████${c2}▐███████▌   ${c4}▐█████▌${rst}
+${pad}   ${c1}▐██████${c2}▐████${c3}▐█████▌${c4}▐█████▌${rst}
+${pad}   ${c1}▐██████${c2}▐████${c3}▐█████▌${c4}███████${rst}
+${pad}${c0}█████▌${c1}████${c2}▐████${c3}▐█████▌${c4}████${c5}▐████${rst}
+${pad}${c0}█████▌${c1}████${c2}▐████${c3}▐█████▌${c4}████${c5}▐████${rst}
 
 EOF
+
+}
+main "$@"
